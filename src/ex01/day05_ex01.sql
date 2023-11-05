038f30fad134d2b476f5c46ae86a38892317b5ba
bonnyped <bonnyped@student.21-school.ru>
2023-11-05 19:48:52 +0600

    minor changes

---
+++ src/ex01/day05_ex01.sql
@@ -0,0 +1,12 @@
+EXPLAIN ANALYZE
+SELECT mn.pizza_name,
+       zz.name
+FROM menu mn
+         JOIN pizzeria zz ON mn.pizzeria_id = zz.id;
+
+SET enable_seqscan = FALSE;
+EXPLAIN ANALYZE
+SELECT mn.pizza_name,
+       zz.name
+FROM menu mn
+         JOIN pizzeria zz ON mn.pizzeria_id = zz.id;
